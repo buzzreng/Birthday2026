d2dc25016f04574e6ee42c0833093274fd9e21a3
buzzreng <mhrbsr03@gmail.com>
2026-05-05 21:57:10 +0700

    Update cause.js

--- cause.js
+++ cause.js
@@ -1,24 +1,20 @@
  // Reasons database
  const reasons = [
     { 
-        text: "You’re such a kind and wonderful person, and I feel lucky to share such a good bond with you. 💖", 
+        text: "Happy Birthday! I was going to send you a hug in a box, but the post office said I was too weird and it wouldn't fit 🥴", 
         emoji: "🌟",
-        gif: "gif1.gif"
     },
     { 
-        text: "May your day be filled with love, laughter, and endless joy. 🌸 ", 
+        text: "May your day be filled with love, laughter, and endless joy 😽 ", 
         emoji: "💗",
-        gif: "gif2.gif"
     },
     { 
-        text: "Wishing you success, happiness, and everything your heart desires. ✨ ", 
+        text: "Wishing you massive success in everything! Just remember, success is 10% hard work and 90% having a cool partner like me to keep you sane 🤪", 
         emoji: "💕",
-        gif: "gif1.gif"
     },
     { 
-        text: "Stay the amazing girl you are—always spreading positivity around. Have the happiest year ahead! 🥳 ", 
+        text: "I love you more than I love a stable internet connection—cheers to another year of making memories together! 🤟", 
         emoji: "🌟",
-        gif: "gif2.gif"
     }
 ];
 
