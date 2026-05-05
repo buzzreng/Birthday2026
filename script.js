376ccd8948e590bfdacb87de464f493c85f77227
buzzreng <mhrbsr03@gmail.com>
2026-05-05 21:32:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('mousemove', (e) => {
 });
 
 // Typing effect for greeting
-const greetingText = "Hey You Know What! You're the most adorable human i ever met! 💖";
+const greetingText = "To the person who makes everything better, I'm so glad you were born! 🎂";
 const greetingElement = document.querySelector('.greeting');
 let charIndex = 0;
 
